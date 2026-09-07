8005e8a8afd85e4be66cce8b005f82ff32c444b6
mayobimu <mayasimizu@gmail.com>
2026-09-07 15:28:37 +0900

    Add Ogp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,29 @@ export const metadata: Metadata = {
   metadataBase: new URL("https://mayobimu.com"),
   title: "mayobimu｜やさしさが、めぐっていくように。",
   description: "Webエンジニアのmayobimuが、技術や暮らしについて書いたり発信したりする個人サイトです。",
+  openGraph: {
+    type: "website",
+    locale: "ja_JP",
+    url: "https://mayobimu.com/",
+    siteName: "mayobimu",
+    title: "mayobimu｜やさしさが、めぐっていくように。",
+    description: "Webエンジニアのmayobimuが、技術や暮らしについて書いたり発信したりする個人サイトです。",
+    images: [{
+      url: "/images/ogp.png",
+      width: 1731,
+      height: 909,
+      alt: "やさしさが、めぐっていくように。mayobimu.comと、パソコンを持ったキャラクター",
+    }],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "mayobimu｜やさしさが、めぐっていくように。",
+    description: "Webエンジニアのmayobimuが、技術や暮らしについて書いたり発信したりする個人サイトです。",
+    images: [{
+      url: "/images/ogp.png",
+      alt: "やさしさが、めぐっていくように。mayobimu.comと、パソコンを持ったキャラクター",
+    }],
+  },
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
